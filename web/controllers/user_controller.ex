e98549b69c0cce78f8ac8f5028d3c486e0a1ef5b
Vrymel Omandam <vrymel.eeweb@gmail.com>
2017-07-11 17:02:47 +0800

    Added form to create a new user

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,7 +1,34 @@
 defmodule HelloPhoenix.UserController do
     use HelloPhoenix.Web, :controller
 
+    alias HelloPhoenix.User
+
     def index(conn, _params) do 
         render conn, :index
+    end
+
+    def new(conn, _params) do
+        changeset = User.changeset(%User{})
+
+        render conn, :new, changeset: changeset
+    end
+
+    def create(conn, %{"user" => param_user}) do
+        changeset = User.changeset(%User{}, param_user)
+
+        case Repo.insert(changeset) do
+            {:ok, user} ->
+                conn
+                |> put_flash(:info, "User successfully created!")
+                |> redirect(to: user_path(conn, :show, user.id))
+        end
+
+        render conn, :index
+    end
+
+    def show(conn, _params) do
+        # TODO: catch id and display user details in template
+
+        render conn, :show
     end
 end
