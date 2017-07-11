edc1197a68dc5840803ddd30f2b14615c39db1e5
Vrymel Omandam <vrymel.eeweb@gmail.com>
2017-07-11 15:43:00 +0800

    Added users table migration

---
+++ priv/repo/migrations/20170711073605_add_users_table.exs
@@ -0,0 +1,13 @@
+defmodule HelloPhoenix.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do 
+      add :email, :string
+      add :password, :string
+      add :display_name, :string
+
+      timestamps()
+    end
+  end
+end
