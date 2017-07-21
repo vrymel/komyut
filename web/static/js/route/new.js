b7f65e3b7a8e6ad38d0a64f974d0e267d93c1d18
Vrymel Omandam <vrymel.dev@gmail.com>
2017-07-21 22:32:50 +0800

    Added event handler for map clicks for creating route waypoints

--- web/static/js/route/new.js
+++ web/static/js/route/new.js
@@ -1,32 +1,52 @@
-function init() {
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
+import { first, last, slice } from "lodash/fp"
 
-    var map = new google.maps.Map(document.getElementById("map"), {
+const directionsService = new google.maps.DirectionsService
+const directionsDisplay = new google.maps.DirectionsRenderer
+const waypoints = []
+
+function init() {
+    const map = new google.maps.Map(document.getElementById("map"), {
         center: new google.maps.LatLng(8.48379, 124.6509111),
         zoom: 16
     });
-
+    
     directionsDisplay.setMap(map);
     
+    map.addListener("click", function(data) {
+        const { latLng } = data;
+        const lat = latLng.lat();
+        const lng = latLng.lng();
+
+        const mapLatLng = new google.maps.LatLng(lat, lng);
+
+        waypoints.push(mapLatLng);
+        updateRouteMap();
+    });
+}
+
+function updateRouteMap() {
+    if (waypoints.length < 2) 
+        return;
+
+    const f = first(waypoints)
+    const l = last(waypoints);
+    const m = slice(1, waypoints.length - 1)(waypoints);
+
     directionsService.route({
-        origin: new google.maps.LatLng(8.484289, 124.646984),
-        destination: new google.maps.LatLng(8.482549, 124.653099),
-        waypoints: [
-            {
-                location: new google.maps.LatLng(8.481052, 124.647991)
-            },
-            {
-                location: new google.maps.LatLng(8.479630, 124.651124)
+        origin: f,
+        destination: l,
+        waypoints: m.map((mapLatLong) => { 
+            return {
+                location: mapLatLong 
             }
-        ],
-        optimizeWaypoints: true,
+        }),
         travelMode: "DRIVING"
     }, function(response, status) {
         if (status === "OK") {
             directionsDisplay.setDirections(response);
         }
     });
+    
 }
 
 export default init;
