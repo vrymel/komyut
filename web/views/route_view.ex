4834b16a1dbf8c10711417a07dc38ec9844a2ff4
Vrymel Omandam <vrymel.dev@gmail.com>
2017-08-05 20:46:44 +0800

    Dynamic hero section title

--- web/views/route_view.ex
+++ web/views/route_view.ex
@@ -1,3 +1,15 @@
 defmodule HelloPhoenix.RouteView do
     use HelloPhoenix.Web, :view
+
+    def page_title("new.html", _assigns) do
+	  	"Create new jeepney route"
+	end
+
+	def page_title("index.html", _assigns) do
+		"Jeepney Routes"
+	end
+
+	def page_title("show.html", %{conn: %Plug.Conn{assigns: %{route: route}}}) do
+		route.description
+	end
 end
