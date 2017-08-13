f3eb6a7c4236c33c1ae1ea7ce681cce2429640fb
Vrymel Omandam <vrymel.dev@gmail.com>
2017-08-13 11:25:55 +0800

    Use polylines instead of waypoints for route path

--- web/static/js/route/new.js
+++ web/static/js/route/new.js
@@ -8,6 +8,7 @@ const directionsDisplay = new google.maps.DirectionsRenderer;
 let map = null;
 let pageRoutes = null;
 let waypointsList;
+let routhPath;
 
 
 function init(configPageRoutes) {
@@ -16,9 +17,15 @@ function init(configPageRoutes) {
         center: new google.maps.LatLng(8.48379, 124.6509111),
         zoom: 16
     });
-    
-    directionsDisplay.setMap(map);
-    
+
+    routhPath = new google.maps.Polyline({
+		strokeColor: '#FF0000',
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	});
+
+	routhPath.setMap(map);
+
     attachEventHandlers();
     initWaypointList();
 }
@@ -30,85 +37,59 @@ const initWaypointList = () => {
             waypoints: []
         },
         methods: {
-            removeWaypoint: function (waypoint){
+            removeWaypoint: function (waypoint, index){
                 this.waypoints = this.waypoints.filter((w) => {
                     return w !== waypoint;
                 });
-                updateRouteMap();
+
+                const path = routhPath.getPath();
+                path.removeAt(index);
             }
         }
     });
 };
 
 function attachEventHandlers() {
-    map.addListener("click", function(data) {
-        const { latLng } = data
-        const lat = latLng.lat()
-        const lng = latLng.lng()
-        
-        const mapLatLng = new google.maps.LatLng(lat, lng)
+    map.addListener("click", function(event) {
+        const rPath = routhPath.getPath();
+        rPath.push(event.latLng);
 
-        waypointsList.waypoints.push(mapLatLng)
-        updateRouteMap();
+
+        waypointsList.waypoints.push(event.latLng);
     });
-    
+
     document.getElementById("submit").addEventListener("click", onSubmitClick)
 }
 
 function onSubmitClick(event) {
     const routeDescription = document.getElementById("route_description").value
-    
+
     const payload = {
         "description": routeDescription,
         "waypoints": waypointsList.waypoints.map((w) => w.toJSON())
     }
-    
+
     fetch(pageRoutes.CREATE, {
         "method": "POST",
         "headers": {
-            "x-csrf-token": getCSRFToken(), // needed for phoenix 
+            "x-csrf-token": getCSRFToken(), // needed for phoenix
             "Content-Type": "application/json"
         },
         "body": JSON.stringify(payload),
         "credentials": "same-origin" // needed for x-csrf-token to work
     }).then(() => {
         waypointsList.waypoints = [];
-        directionsDisplay.set('directions', null);
-        updateRouteMap();
-        // alert("Route created!")
+
+        const path = routhPath.getPath();
+        while(path.length)
+        	path.removeAt(0)
+
+        alert("Route created!");
     })
 }
 
 function getCSRFToken() {
     return document.querySelector("input[name=_csrf_token]").value
-}
-
-function updateRouteMap() {
-    if (waypointsList.waypoints.length < 2) {
-        return;
-    }
-
-    const waypoints = waypointsList.waypoints;
-    
-    const f = first(waypoints)
-    const l = last(waypoints);
-    const m = slice(1, waypoints.length - 1)(waypoints);
-    
-    directionsService.route({
-        origin: f,
-        destination: l,
-        waypoints: m.map((mapLatLong) => { 
-            return {
-                location: mapLatLong 
-            }
-        }),
-        travelMode: "DRIVING"
-    }, function(response, status) {
-        if (status === "OK") {
-            directionsDisplay.setDirections(response);
-        }
-    });
-    
 }
 
 export default init;
