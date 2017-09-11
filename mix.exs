c8fc50f6793776d49d290d8fc9587309b02a2966
Vrymel Omandam <vrymel.dev@gmail.com>
2017-09-11 21:20:33 +0800

    Release files

--- mix.exs
+++ mix.exs
@@ -37,7 +37,8 @@ defmodule WaypointsDirect.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:distillery, "~> 1.4", runtime: false}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
