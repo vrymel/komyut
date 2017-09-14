ffad6b3e62b5e833ef04fc92966c803fc2e177b7
Vrymel Omandam <vrymel.dev@gmail.com>
2017-09-14 19:09:44 +0800

    Removed unused action in route_controller

--- web/controllers/route_controller.ex
+++ web/controllers/route_controller.ex
@@ -14,26 +14,4 @@ defmodule WaypointsDirect.RouteController do
 
         render conn, :new, changeset: changeset
     end
-
-    def create(conn, %{"route" => route_param}) do
-        changeset = Route.changeset(%Route{}, route_param)
-
-        case Repo.insert(changeset) do
-            {:ok, route} ->
-                conn 
-                |> put_flash(:info, "New route created!")
-                |> redirect(to: route_path(conn, :show, route.id))
-            {:error, error_changeset} ->
-                IO.inspect(error_changeset)
-                conn 
-                |> put_flash(:error, "Could not create route, please try again")
-                |> render(:new, changeset: error_changeset)
-        end
-    end
-
-    def show(conn, %{"id" => route_id}) do
-        route = Repo.get!(Route, route_id)
-
-        render conn, :show, route: route
-    end
 end
