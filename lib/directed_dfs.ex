aa527b9218e0bd229266dc5f514d07f7a708eb53
Vrymel Omandam <vrymel.dev@gmail.com>
2017-10-03 18:31:39 +0800

    [WIP] Work on dfs recursion function

--- lib/directed_dfs.ex
+++ lib/directed_dfs.ex
@@ -3,21 +3,46 @@ defmodule WaypointsDirect.DirectedDFS do
     alias WaypointsDirect.Graph
     
     def process_graph(graph, source) do
-        %{:marked => init_marker(graph)}
+        marker = %{:marked => init_marker(graph)}
+
+        dfs(graph, source, marker)
     end
 
     defp init_marker(graph) do
-        do_init_marker(0, graph.vertices_count - 1, %{})    
+        do_init_marker(%{}, 0, graph.vertices_count - 1)    
     end
 
-    defp do_init_marker(current_count, count_limit, marker) do
+    defp do_init_marker(marker, current_count, count_limit) do
         if current_count <= count_limit do
-            do_init_marker(current_count + 1, count_limit, Map.put(marker, current_count, false))
+            do_init_marker(Map.put(marker, current_count, false), current_count + 1, count_limit)
         else
             marker
         end
     end
 
-    defp dfs(graph, vertex, marker) do
+    defp dfs(graph, vertex, %{:marked => marked} = marker) do
+        marker = %{:marked => %{marked | vertex => true}}
+        adjacent = Graph.adjacent(graph, vertex)
+        [head | tail] = adjacent
+
+        if !marked[head] do
+            dfs(graph, head, marker)
+        else
+            do_dfs(graph, tail, marker)
+        end
+    end
+
+    defp do_dfs(graph, adjacent, %{:marked => marked} = marker) do
+        [head | tail] = adjacent
+
+        if !marked[head] do
+            dfs(graph, head, marker)
+        end
+
+        if Enum.empty? tail do
+            marker
+        end
+
+        do_dfs(graph, tail, marker)
     end
 end
