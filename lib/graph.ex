966a4e6f62215a7ca055f8cc3c4fbee461c55fa0
Vrymel Omandam <vrymel.dev@gmail.com>
2017-12-17 15:07:22 +0800

    Updated Graph.add_edge to accept route_edge

--- lib/graph.ex
+++ lib/graph.ex
@@ -1,28 +1,25 @@
 defmodule WaypointsDirect.Graph do
 
-    def new(vertices) do
-        bags = init_bag(vertices)
+    alias WaypointsDirect.RouteEdge
 
-        %{:vertices_count => vertices, :edges_count => 0, :bags => bags}
+    def new() do
+        %{:vertices_count => 0, :edges_count => 0, :bags => %{}}
     end
 
-    defp init_bag(vertices) do
-        do_init_bag(0, vertices - 1, %{})
-    end
+    def add_edge(%{:bags => bags, :edges_count => edges_count} = graph, %RouteEdge{from_intersection_id: from_id, to_intersection_id: to_id}) do
+        result = Map.get(bags, from_id)
+        source_bag = unless result, do: [], else: result
 
-    defp do_init_bag(current_vertex, vertex_limit, bag) do
-        if current_vertex <= vertex_limit do
-            do_init_bag(current_vertex + 1, vertex_limit, Map.put(bag, current_vertex, [])) 
+        proceed_add = Enum.member?(source_bag, to_id)
+        source_bag = unless proceed_add, do: [to_id | source_bag], else: source_bag
+
+        unless proceed_add do
+            new_bags = Map.put(bags, from_id, source_bag)
+
+            %{graph | :bags => new_bags, :edges_count => edges_count + 1}
         else
-            bag
+            graph
         end
-    end
-
-    def add_edge(%{:bags => bags, :edges_count => edges_count} = graph, v, w) do
-        %{^v => v_bag} = bags
-        bags = bags |> put_in([v], [w | v_bag])
-
-        %{graph | :bags => bags, :edges_count => edges_count + 1}
     end
 
     def adjacent(%{:bags => bags}, vertex) do
