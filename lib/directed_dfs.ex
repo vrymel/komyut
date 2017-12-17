647f05bc5d6038b237c6133c1f891ddc1fa24a2a
Vrymel Omandam <vrymel.dev@gmail.com>
2017-12-17 16:02:59 +0800

    Changed directed dfs process to lazy init the vertex marker

--- lib/directed_dfs.ex
+++ lib/directed_dfs.ex
@@ -3,29 +3,21 @@ defmodule WaypointsDirect.DirectedDFS do
     alias WaypointsDirect.Graph
     
     def process_graph(graph, source) do
-        marker = %{:marked => init_marker(graph)}
-
-        dfs(graph, source, marker)
-    end
-
-    defp init_marker(graph) do
-        do_init_marker(%{}, 0, graph.vertices_count - 1)    
-    end
-
-    defp do_init_marker(marker, current_count, count_limit) do
-        if current_count <= count_limit do
-            do_init_marker(Map.put(marker, current_count, false), current_count + 1, count_limit)
-        else
-            marker
-        end
+        dfs(graph, source, %{:marked => %{}})
     end
 
     defp dfs(graph, vertex, %{:marked => marked} = marker) do
+        # TODO: do not process vertex if it is already marked as visited
+
+        marked = unless Map.get(marked, vertex), do: Map.put(marked, vertex, false), else: marked
+
         marker = %{:marked => %{marked | vertex => true}}
         adjacent = Graph.adjacent(graph, vertex)
 
         case adjacent do
             [] -> 
+                marker
+            nil ->
                 marker
             _ -> 
                 # TODO: find out why pattern matching is not working here. instead of calling arity [head | tail], this call itself.
