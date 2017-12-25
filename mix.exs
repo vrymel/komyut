48301ec0ce416bed4cebeeb798232be96969546e
Vrymel Omandam <vrymel.dev@gmail.com>
2017-12-26 07:55:15 +0800

    Bump elixir requirement to 1.5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule WaypointsDirect.Mixfile do
   def project do
     [app: :waypoints_direct,
      version: "0.4.1-beta",
-     elixir: "~> 1.2",
+     elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
