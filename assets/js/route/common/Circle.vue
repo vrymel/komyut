ea4a1a5a0e2c02028ae612e64161eb70c6b920f6
Vrymel Omandam <vrymel.dev@gmail.com>
2017-12-31 20:37:03 +0800

    Make Circle center configurable via props

--- assets/js/route/common/Circle.vue
+++ assets/js/route/common/Circle.vue
@@ -4,26 +4,33 @@
 <script>
 export default {
   name: "google-map-circle",
+  props: ['center'],
 
   created() {
     if (this.$parent) {
-      this.mapParent = this.$parent;
+      this._mapParent = this.$parent;
     }
   },
 
   async mounted() {
-      const map = await this.mapParent.getMap();
+      const map = await this._mapParent.getMap();
 
-      var cityCircle = new google.maps.Circle({
+      this._circle = new google.maps.Circle({
         strokeColor: "#FF0000",
         strokeOpacity: 0.8,
         strokeWeight: 2,
         fillColor: "#FF0000",
         fillOpacity: 0.75,
         map: map,
-        center: { lat: 8.48379, lng: 124.6509111 },
+        center: this.center,
         radius: 15
       });
+  },
+
+  beforeDestroy() {
+      if (this._circle) {
+          this._circle.setMap(null);
+      }
   }
 };
 </script>
