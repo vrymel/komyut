395aed7e4d083c9c61fb0507ecc9e923e13af63a
Vrymel Omandam <vrymel.dev@gmail.com>
2018-01-05 16:07:23 +0800

    Add intersection in google map on click

--- assets/js/route/new/AppNewRoute.vue
+++ assets/js/route/new/AppNewRoute.vue
@@ -1,35 +1,61 @@
 <template>
-    <div class="app-new-route">
-        <div class="map">
-            <google-map></google-map>
-        </div>
-        <div class="sidebar">Sidebar is here</div>
+  <div class="app-new-route">
+    <div class="map">
+      <google-map
+      @click="onMapClick">
+        <google-map-circle 
+          v-for="(intersection, index) in intersections"
+          :key="index"
+          :center="intersection"/>
+      </google-map>
     </div>
+    <div class="sidebar">
+      <button @click="remove">Remove</button>
+    </div>
+  </div>
 </template>
 
 <script>
 import Map from "../common/Map.vue";
+import Circle from "../common/Circle.vue";
 
 export default {
-    name: "app-new-route",
+    name: "AppNewRoute",
     components: {
-        'google-map': Map
+        "google-map": Map,
+        "google-map-circle": Circle
+    },
+    data() {
+        return { 
+            intersections: []
+        };
+    },
+    methods: {
+        remove() {
+            this.attach = !this.attach;
+        },
+        onMapClick({ latLng }) {
+            const lat = latLng.lat();
+            const lng = latLng.lng();
+
+            this.intersections.push({lat, lng});
+        }
     }
-}
+};
 </script>
 
 <style lang="scss" scoped>
-    .app-new-route {
-        display: flex;
-        width: 100%;
+.app-new-route {
+  display: flex;
+  width: 100%;
 
-        .map {
-            flex: 3;
-        }
+  .map {
+    flex: 3;
+  }
 
-        .sidebar {
-            flex: 1;
-        }
-    }
+  .sidebar {
+    flex: 1;
+  }
+}
 </style>
 
