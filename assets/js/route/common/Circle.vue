b906a3b95f374a6f2f57951590d40e6af8c3bd97
Vrymel Omandam <vrymel.dev@gmail.com>
2018-01-05 16:08:17 +0800

    Format Circle component according to eslint rules

--- assets/js/route/common/Circle.vue
+++ assets/js/route/common/Circle.vue
@@ -1,36 +1,42 @@
-<template>
-</template>
-
 <script>
 export default {
-  name: "google-map-circle",
-  props: ['center'],
+    name: "GoogleMapCircle",
+    props: {
+        center: {
+            type: Object,
+            required: true
+        }
+    },
 
-  created() {
-    if (this.$parent) {
-      this._mapParent = this.$parent;
+    created() {
+        if (this.$parent) {
+            this._mapParent = this.$parent;
+        }
+    },
+
+    async mounted() {
+        const map = await this._mapParent.getMap();
+
+        this._circle = new google.maps.Circle({
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: "#FF0000",
+            fillOpacity: 0.75,
+            map: map,
+            center: this.center,
+            radius: 15
+        });
+    },
+
+    beforeDestroy() {
+        if (this._circle) {
+            this._circle.setMap(null);
+        }
+    },
+
+    render () {
+        return ''
     }
-  },
-
-  async mounted() {
-      const map = await this._mapParent.getMap();
-
-      this._circle = new google.maps.Circle({
-        strokeColor: "#FF0000",
-        strokeOpacity: 0.8,
-        strokeWeight: 2,
-        fillColor: "#FF0000",
-        fillOpacity: 0.75,
-        map: map,
-        center: this.center,
-        radius: 15
-      });
-  },
-
-  beforeDestroy() {
-      if (this._circle) {
-          this._circle.setMap(null);
-      }
-  }
 };
 </script>
