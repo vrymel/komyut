b53b62447f00a7369f65166d8c50e24db81921cd
Vrymel Omandam <vrymel.dev@gmail.com>
2018-02-03 14:35:39 +0800

    Initial restructure of the index page

---
+++ assets/js/routes/index/AppIndexRoute.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="app-index-route">
+    <div class="map-container">
+      <google-map />
+    </div>
+
+    <div 
+    class="route-context">
+      <div class="app-logo">
+        <img alt="WaypointsDirect">
+      </div>
+
+      <div class="current city">
+        <h6 class="header">City</h6>
+
+        <div class="description">
+          Cagayan de Oro City
+        </div>
+      </div>
+
+      <div class="current route">
+        <h6 class="header">Route</h6>
+
+        <div class="description">
+          {{ description }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Map from "../common/Map";
+import Circle from "../common/Circle";
+
+export default {
+    name: "AppIndexRoute",
+    components: {
+        "google-map": Map,
+        "google-map-circle": Circle
+    },
+    data() {
+        return {};
+    },
+    methods: {
+
+    }
+}
+</script>
---
+++ assets/js/routes/index/main.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import AppIndexRoute from "./AppIndexRoute.vue";
+
+new Vue({
+    el: '#app-root',
+    render: h => h(AppIndexRoute)
+});
