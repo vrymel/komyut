ee95048119511ef65eb43343a923ea044463c357
Vrymel Omandam <vrymel.dev@gmail.com>
2018-02-10 12:58:37 +0800

    Refactored geo point module and added to_degrees helper

--- lib/graph_helpers/geo_point.ex
+++ lib/graph_helpers/geo_point.ex
@@ -1,11 +1,19 @@
 defmodule WaypointsDirect.GeoPoint do
     defstruct latitude: nil, longitude: nil
 
-    def from_degrees(latitude, longitude) do
-        %WaypointsDirect.GeoPoint{latitude: to_radian(latitude), longitude: to_radian(longitude)}
+    def from_degrees(%WaypointsDirect.GeoPoint{:latitude => lat, :longitude => lng}) do
+        %WaypointsDirect.GeoPoint{latitude: to_radian_value(lat), longitude: to_radian_value(lng)}
     end
 
-    defp to_radian(degrees) do 
+    def to_degrees(%WaypointsDirect.GeoPoint{:latitude => lat, :longitude => lng}) do
+        %WaypointsDirect.GeoPoint{latitude: to_degree_value(lat), longitude: to_degree_value(lng)}
+    end
+
+    defp to_radian_value(degrees) do 
         (degrees * :math.pi) / 180
+    end
+
+    defp to_degree_value(radians) do
+        radians * 180 / :math.pi
     end
 end
