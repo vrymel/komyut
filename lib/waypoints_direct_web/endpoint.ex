711cd008669e1c4dcd4a765b3d4b2604af2a21ea
Vrymel Omandam <vrymel.dev@gmail.com>
2018-02-25 14:08:04 +0800

    Added raven js library and enabled lib endpoint

--- lib/waypoints_direct_web/endpoint.ex
+++ lib/waypoints_direct_web/endpoint.ex
@@ -9,7 +9,7 @@ defmodule WaypointsDirectWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :waypoints_direct, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt .well-known)
+    only: ~w(css fonts images js lib favicon.ico robots.txt .well-known)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
