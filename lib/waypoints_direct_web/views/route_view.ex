431b6df50313d6412f28f6985878b6daf2dd9e1f
Vrymel Omandam <vrymel.dev@gmail.com>
2018-02-25 14:19:34 +0800

    Added raven

--- lib/waypoints_direct_web/views/route_view.ex
+++ lib/waypoints_direct_web/views/route_view.ex
@@ -4,4 +4,8 @@ defmodule WaypointsDirectWeb.RouteView do
     def googleMapApiKey do
         System.get_env("GOOGLE_MAP_API_KEY")
     end
+
+    def sentryClientDsn do
+        System.get_env("SENTRY_CLIENT_DSN")
+    end
 end
