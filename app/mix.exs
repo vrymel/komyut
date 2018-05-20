ff3a2c592c525d4b311c6d82b3ad864b5dc050fb
Vrymel <vrymel.dev@gmail.com>
2018-05-20 16:02:52 +0800

    Bump version to 1.0.0

--- app/mix.exs
+++ app/mix.exs
@@ -3,7 +3,7 @@ defmodule WaypointsDirect.Mixfile do
 
   def project do
     [app: :waypoints_direct,
-     version: "0.5.0-beta",
+     version: "1.0.0",
      elixir: "~> 1.5",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
