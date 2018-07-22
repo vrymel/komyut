a2394259e3583a401eabb1d2c4e2da5b01f2393b
Vrymel <vrymel.dev@gmail.com>
2018-07-22 15:08:41 +0800

    Added run_phxserver for running mix phx.server with inline flags

---
+++ run_phxserver.py
@@ -0,0 +1,22 @@
+import subprocess
+
+def get_flags():
+    flags = []
+    with open('.env') as f:
+        for line in f:
+            line = line.strip()
+            # Ignore comments
+            if not line.startswith('#'):
+                flags.append(line)
+
+    return ' '.join(flags)
+
+flags = get_flags()
+change_dir = 'cd app'
+mix_command = 'mix phx.server'
+main_command = '{} {}'.format(flags, mix_command)
+
+final_command = '{} && {}'.format(change_dir, main_command)
+
+print("Running mix phx.server")
+subprocess.call(final_command, shell=True)
